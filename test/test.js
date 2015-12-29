f7c7ec9421170b402d522c1428f49c1fefba76a1
Mithun Dhiman <mi2oon@gmail.com>
2015-12-29 17:47:56 +0100

    Tests added

---
+++ test/test.js
@@ -0,0 +1,69 @@
+import path     from 'path';
+import test     from 'ava';
+import helpers  from 'yeoman-test';
+import assert   from 'yeoman-assert';
+import pify     from 'pify';
+
+let generator;
+
+test.beforeEach(async () => {
+    await pify(helpers.testDirectory)(path.join(__dirname, 'temp'));
+    generator = helpers.createGenerator('rammevaerk:app', ['../../app'], null, { 'skipInstall': true, 'keep-silence': true });
+});
+
+test.serial('generates expected files', async () => {
+
+    helpers.mockPrompt(generator, {
+        projectName: 'avaTest'
+    });
+
+    await pify(generator.run.bind(generator))();
+
+    const expected = [
+        '.editorconfig',
+        '.eslintignore',
+        '.eslintrc',
+        '.sass-lint.yml',
+        'gulpfile.js',
+        'package.json',
+        'README.md',
+        'AvaTest.sublime-project',
+    ];
+
+    assert.file(expected);
+});
+
+test.serial('generates expected dependencies', async () => {
+
+    helpers.mockPrompt(generator, {
+        projectName: 'avaTest',
+        projectModules: ['lightbox']
+    });
+
+    await pify(generator.run.bind(generator))();
+
+    assert.JSONFileContent('package.json', {"name": "AvaTest"});
+    assert.fileContent('package.json', '"fancyBox"');
+    assert.noFileContent('package.json', '"cookieConsent"');
+
+    const unExpected = [
+        'AvaTest.web/Mockup/cookie.jade',
+        'AvaTest.web/Styles/Shared/Vendor/cookieconsent.scss',
+        'AvaTest.web/Scripts/Shared/cookieconsent/index.js'
+    ];
+
+    assert.noFile(unExpected);
+});
+
+
+test.serial('generates kebabcase name', async () => {
+
+    helpers.mockPrompt(generator, {
+        projectName: 'a v a t e s t'
+    });
+
+    await pify(generator.run.bind(generator))();
+
+    assert.file('A-v-a-t-e-s-t.sublime-project');
+
+});

c6900da99253714ee06c512f84c64079c1845667
Mithun Dhiman <mi2oon@gmail.com>
2016-10-19 20:50:42 +0200

    Lightbox iframe updated

--- app/templates/project-name.Website/Scripts/Shared/lightbox/lightbox.iframe.js
+++ app/templates/project-name.Website/Scripts/Shared/lightbox/lightbox.iframe.js
@@ -3,26 +3,37 @@
 import $ from 'jquery';
 import 'fancyBox';
 
-/**
- * [iframeLightbox makes a lightbox with iframe]
- * @param  {[string]}   scope   [css selector]
- * @return {[undefined]}        [nothing returned]
- */
-export default function iframeLightbox(scope){
+const defaults = {
+    width: '75%',
+    height: '75%',
+    padding: 10,
+    type: 'iframe',
+    iframe: {
+        scrolling: 'auto',
+        preload: true
+    },
+    autoSize: false
+};
+
+function lightboxClassInit(scope) {
     $(scope).each(function () {
         const elm = $(this);
-        const opts = Object.assign(elm.data(), {
-            width: '75%',
-            height: '75%',
-            padding: 10,
-            type: 'iframe',
-            iframe: {
-                scrolling: 'auto',
-                preload: true
-            },
-            autoSize: false
-        });
+        const opts = Object.assign(elm.data(), defaults);
 
         elm.fancybox(opts);
     });
+}
+
+function lightboxInit(newOpts) {
+    const opts = Object.assign(newOpts, defaults);
+
+    window.$.fancybox(opts);
+}
+
+export default function iframeLightbox(input){
+    if (typeof input === 'string') {
+        lightboxClassInit(input);
+    } else if (typeof input === 'object') {
+        lightboxInit(input);
+    }
 }

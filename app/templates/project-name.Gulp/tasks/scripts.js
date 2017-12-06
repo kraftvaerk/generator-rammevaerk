dc97121f8587371263b4dc284c0530250a10f4c1
Mithun Dhiman <mi2oon@gmail.com>
2017-12-06 15:49:54 +0100

    ♻️ - Webpack output simplified  + chunk cleaning added

--- app/templates/project-name.Gulp/tasks/scripts.js
+++ app/templates/project-name.Gulp/tasks/scripts.js
@@ -1,24 +1,32 @@
 'use strict';
 
 const gulp            = require('gulp');
+const plumber         = require('gulp-plumber');
+const clean           = require('gulp-rimraf');
 const webpack         = require('webpack');
 const gutil           = require('gulp-util');
+const conf            = require('../config');
 const webpackConfig   = require('../../webpack.config');
 
-gulp.task('scripts', function(callback) {
+gulp.task('scripts:clean', () => {
+    return gulp.src(conf.js.dest + '/*.{js,map,LICENSE}', { read: false })
+        .pipe(plumber())
+        .pipe(clean({force: true}))
+        .on('error', gutil.log);
+});
+
+gulp.task('scripts', ['scripts:clean'], function(callback) {
     webpack(webpackConfig, function(err, stats) {
         if (err){
             throw new gutil.PluginError('webpack', err);
         }
 
         gutil.log('[webpack:build] Completed\n' + stats.toString({
-            assets: true,
-            chunks: false,
-            chunkModules: false,
             colors: true,
-            hash: false,
-            timings: false,
-            version: false
+            modules: false,
+            children: false,
+            chunks: false,
+            chunkModules: false
         }));
 
         callback();

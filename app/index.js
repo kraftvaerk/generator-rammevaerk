563636ce50309119433a63a6cb8e2b1bfe563fbb
Kristofor Salmin <racse1@users.noreply.github.com>
2017-12-18 15:55:15 +0300

    Improve _dotfiles task of generator and clean up a bit

--- app/index.js
+++ app/index.js
@@ -1,11 +1,16 @@
 'use strict';
 
 const _         = require('lodash');
-const yosay     = require('yosay');
-const superb    = require('superb');
-const mkdirp    = require('mkdirp');
 const chalk     = require('chalk');
 const Generator = require('yeoman-generator');
+const mkdirp    = require('mkdirp');
+const superb    = require('superb');
+const yosay     = require('yosay');
+
+const OPTIONS = {
+    KEEP_SILENCE: 'keep-silence',
+    SKIP_INSTALL: 'skip-install'
+};
 
 function capitalizeKebabCase(name) {
     return _.reduce(name.split('-'), (acc, str, index) => {
@@ -14,47 +19,38 @@ function capitalizeKebabCase(name) {
     }, '');
 }
 
-
 module.exports = class extends Generator {
     constructor(...args) {
         // Call the parent constructor
         super(...args);
 
-        this.option('keep-silence', {
-            desc: 'Test framework to be invoked',
+        this.answers = {};
+
+        this.option(OPTIONS.SKIP_INSTALL, {
+            desc: 'Do not install npm dependencies',
             type: Boolean
         });
 
-        this.option('babel', {
-            desc: 'Use Babel',
-            type: Boolean,
-            defaults: true
+        this.option(OPTIONS.KEEP_SILENCE, {
+            desc: 'Do not log output into console',
+            type: Boolean
         });
     }
 
     initializing() {
-        this.option('keep-silence', {
-            desc: 'Test framework to be invoked',
-            type: Boolean
-        });
-
-
-        this.projectYear = new Date().getFullYear();
-        this.answers = {};
-
-        if (!this.options['keep-silence']){
+        if (!this.options[OPTIONS.KEEP_SILENCE]){
             this.log(yosay(
-                chalk.yellow(`Initializing a ${superb()} new rammevaerk project`)
+                chalk.yellow(`Initializing a ${superb()} new rammevaerk project...`)
             ));
         }
     }
 
     prompting() {
-        let questions = [];
+        const questions = [];
 
-        if (!this.options['keep-silence']){
+        if (!this.options[OPTIONS.KEEP_SILENCE]){
             this.log('-------------------------------------------------------');
-            this.log('Please answers the following questions.');
+            this.log('Please answers the following questions:');
             this.log('-------------------------------------------------------\n');
         }
 
@@ -65,7 +61,7 @@ module.exports = class extends Generator {
             message: chalk.yellow('What is your project name?'),
             default: _.kebabCase(this.appname),
             filter: _.kebabCase,
-            validate: (name) => name.length > 0
+            validate: name => name.length > 0
         });
 
         // Project description
@@ -93,12 +89,13 @@ module.exports = class extends Generator {
         });
 
         return this.prompt(questions).then((answers) => {
-            let gitName = this.user.git.name() ? ` — ${this.user.git.name().split(' ')[0]}` : '.';
+            const gitName = this.user.git.name() ? ` — ${this.user.git.name().split(' ')[0]}` : '.';
 
             this.answers = answers;
             this.answers.projectName = capitalizeKebabCase(this.answers.projectName);
-            this.answers.projectDescription = this.answers.projectDescription === 'N/A' ? `I promise to add the description later on to this ${superb()} new project${gitName}` : this.answers.projectDescription;
-
+            this.answers.projectDescription = this.answers.projectDescription === 'N/A'
+                ? `I promise to add the description later on to this ${superb()} new project${gitName}.`
+                : this.answers.projectDescription;
         });
     }
 
@@ -109,37 +106,44 @@ module.exports = class extends Generator {
             '.Website/Assets',
             '.Website/Content',
             '.Website/Fonts',
-            '.Website/Styles',
-            '.Website/Styles/' + this.answers.projectName,
-            '.Website/Styles/Shared',
             '.Website/Scripts',
             '.Website/Scripts/' + this.answers.projectName,
-            '.Website/Scripts/Shared'
+            '.Website/Scripts/Shared',
+            '.Website/Styles',
+            '.Website/Styles/' + this.answers.projectName,
+            '.Website/Styles/Shared'
         ];
 
-        if (!this.options['keep-silence']){
+        if (!this.options[OPTIONS.KEEP_SILENCE]){
             this.log('\n');
             this.log.ok('Setting up directories...');
         }
 
-        folders.forEach((folder) => mkdirp.sync(this.answers.projectName + folder));
+        folders.forEach((folder) => {
+            mkdirp.sync(`${this.answers.projectName}${folder}`);
+        });
 
         done();
     }
     _dotfiles(){
         const done = this.async();
+        const dotfiles = [
+            'babelrc',
+            'browserslistrc',
+            'editorconfig',
+            'eslintrc',
+            'gitattributes',
+            'gitignore',
+            'stylelintrc'
+        ];
 
-        if (!this.options['keep-silence']){
+        if (!this.options[OPTIONS.KEEP_SILENCE]){
             this.log.ok('Setting up the dotfiles...');
         }
 
-        this.fs.copy(this.templatePath('_babelrc'), this.destinationPath('.babelrc'));
-        this.fs.copy(this.templatePath('_browserslistrc'), this.destinationPath('.browserslistrc'));
-        this.fs.copy(this.templatePath('_editorconfig'), this.destinationPath('.editorconfig'));
-        this.fs.copy(this.templatePath('_eslintrc'), this.destinationPath('.eslintrc'));
-        this.fs.copy(this.templatePath('_gitattributes'), this.destinationPath('.gitattributes'));
-        this.fs.copy(this.templatePath('_gitignore'), this.destinationPath('.gitignore'));
-        this.fs.copy(this.templatePath('_stylelintrc'), this.destinationPath('.stylelintrc'));
+        dotfiles.forEach((file) => {
+            this.fs.copy(this.templatePath(`_${file}`), this.destinationPath(`.${file}`));
+        });
 
         done();
     }
@@ -147,20 +151,15 @@ module.exports = class extends Generator {
         const done = this.async();
         let readme;
 
-        if (!this.options['keep-silence']){
-            this.log.ok('Setting up the readme file...');
+        if (!this.options[OPTIONS.KEEP_SILENCE]){
+            this.log.ok('Setting up the README.md...');
         }
 
         readme = this.fs.read(this.templatePath('README/_BODY.md'));
-
         readme += '\n';
-
         readme += this.fs.read(this.templatePath('README/_IDE_VISUAL_STUDIO.md'));
-
         readme += '\n';
-
         readme += this.fs.read(this.templatePath('README/_USAGE.md'));
-
         readme = _.template(readme)(this);
 
         this.fs.write(this.destinationPath('README.md'), readme);
@@ -170,12 +169,11 @@ module.exports = class extends Generator {
     _gulp() {
         const done = this.async();
 
-        if (!this.options['keep-silence']){
+        if (!this.options[OPTIONS.KEEP_SILENCE]){
             this.log.ok('Setting up the Gulp...');
         }
 
         this.fs.copyTpl(this.templatePath('project-name.Gulp'), this.destinationPath(`${this.answers.projectName}.Gulp`), this);
-
         this.fs.copyTpl(this.templatePath('_gulpfile.js'), this.destinationPath('gulpfile.js'), this);
         this.fs.copyTpl(this.templatePath('_webpack.config.js'), this.destinationPath('webpack.config.js'), this);
         done();
@@ -184,7 +182,7 @@ module.exports = class extends Generator {
         const done = this.async();
         let pkg = {};
 
-        if (!this.options['keep-silence']){
+        if (!this.options[OPTIONS.KEEP_SILENCE]){
             this.log.ok('Setting up the package.json...');
         }
 
@@ -242,8 +240,7 @@ module.exports = class extends Generator {
     _projectFiles() {
         const done = this.async();
 
-
-        if (!this.options['keep-silence']){
+        if (!this.options[OPTIONS.KEEP_SILENCE]){
             this.log.ok('Setting up the project files...');
         }
 
@@ -251,7 +248,6 @@ module.exports = class extends Generator {
             this.templatePath('project-name.Website/favicon.ico'),
             this.destinationPath(`${this.answers.projectName}.Website/favicon.ico`)
         );
-
 
         // Scripts
         this.fs.copy(
@@ -301,16 +297,14 @@ module.exports = class extends Generator {
     }
 
     install() {
-        if (!this.options['skip-install']){
+        if (!this.options[OPTIONS.SKIP_INSTALL]){
             this.spawnCommandSync('npm', ['update', '--save', '--save-exact']);
             this.spawnCommandSync('npm', ['update', '--save-dev', '--save-exact']);
         }
-
-        this.installDependencies({bower: false});
     }
 
     end() {
-        if (!this.options['keep-silence']){
+        if (!this.options[OPTIONS.KEEP_SILENCE]){
             this.log.writeln('\n-------------------------------------------------------');
             this.log.writeln(`All done, you can ${chalk.green.bold('npm run watch')}, to start dev server.`);
             this.log.writeln('-------------------------------------------------------\n');
--- test/test.js
+++ test/test.js
@@ -1,11 +1,14 @@
-const path =     require('path');
-const helpers =  require('yeoman-test');
-const assert =   require('yeoman-assert');
+'use strict';
+
+const assert  = require('yeoman-assert');
+const helpers = require('yeoman-test');
+const path    = require('path');
 
 describe('rammevaerk:app', () => {
     beforeAll(() => {
-        return helpers.run(path.join(__dirname, '../app'))
-            .withPrompts({projectName: 't e s t', 'skip-install': true, 'keep-silence': true });
+        return helpers
+            .run(path.join(__dirname, '../app'))
+            .withPrompts({ projectName: 't e s t', 'skip-install': true, 'keep-silence': true });
     });
 
     it('generates expected files', () => {
@@ -25,10 +28,10 @@ describe('rammevaerk:app', () => {
     });
 
     it('generates capitalized kebabcase name', () => {
-        assert.JSONFileContent('package.json', {'name': 'T-e-s-t' });
+        assert.JSONFileContent('package.json', { name: 'T-e-s-t' });
     });
 
     it('generates expected dependencies', () => {
-        assert.JSONFileContent('package.json', {'dependencies': { 'jquery': 'latest' }});
+        assert.JSONFileContent('package.json', { dependencies: { jquery: 'latest' } });
     });
 });

73f354b5f5d40a35d53129598ecb85c996bb35d5
Mithun Dhiman <mi2oon@gmail.com>
2018-02-25 12:48:50 +0100

    🐛 - Fixing selector + display style

--- app/templates/project-name.Website/Scripts/Shared/cookieconsent/index.js
+++ app/templates/project-name.Website/Scripts/Shared/cookieconsent/index.js
@@ -1,7 +1,7 @@
 import CookieConsent from 'kv.cookieconsent';
 
 export function init(selector){
-    if (!document.getElementById(selector)){
+    if (!document.querySelector(selector);){
         return;
     }
 

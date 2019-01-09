6e9e30218dacf80f9b1de613ff4328eeaf226183
Mithun Dhiman <mi2oon@gmail.com>
2019-01-09 15:04:52 +0100

    Avoid running script build twice on watch + production mode fix

--- app/templates/_gulpfile.js
+++ app/templates/_gulpfile.js
@@ -4,14 +4,13 @@ import HubRegistry from 'gulp-hub';
 import server from './<%= answers.projectName %>.Gulp/tasks/browser-sync.js';
 import watcher from './<%= answers.projectName %>.Gulp/tasks/watch.js';
 
-global.production = process.env.NODE_ENV === 'production';
-
 // Load some files into the registry
 const hub = new HubRegistry(['./<%= answers.projectName %>.Gulp/tasks/*.js']);
 
 // Tell gulp to use the tasks just loaded
 gulp.registry(hub);
 
+const production = process.env.NODE_ENV === 'production';
 const tasks = [
     'scripts',
     'styles',
@@ -23,5 +22,5 @@ if (!production) {
 }
 
 gulp.task('default', gulp.parallel(tasks));
-gulp.task('server', gulp.series('default', server.serve));
+gulp.task('server', gulp.series(gulp.parallel(tasks.filter(task => task !== 'scripts')), server.serve));
 gulp.task('watch', gulp.series('watch:flag', 'server', watcher.watch));

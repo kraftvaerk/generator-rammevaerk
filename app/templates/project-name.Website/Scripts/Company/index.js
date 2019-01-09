8f43689bcbc612e2b8cea31d6d571c22bfa003b4
Mithun Dhiman <mi2oon@gmail.com>
2019-01-09 15:01:31 +0100

    lightbox dynamically loaded

--- app/templates/project-name.Website/Scripts/Company/index.js
+++ app/templates/project-name.Website/Scripts/Company/index.js
@@ -1,29 +1,30 @@
-'use strict';
-
-import $                from 'jquery';
-
-import cookieConsent    from '../Shared/cookieconsent/index';
-import lightbox         from '../Shared/lightbox/index';
+import $ from 'jquery';
+import cookieConsent from '@/Shared/cookieconsent/index';
 
 // Expose jQuery to global scope
 global.$ = global.jQuery = $;
 
-//Add an alias to the App, if we need to call App from the outside:
+// Add an alias to the App, if App if needed to be called from outside
 window.App = window.App || {};
 
 window.App.common = {
+    dynamicImportModules() {
+        const lightBoxElm = document.querySelector('[class*=js-lightbox]');
+        if (lightBoxElm) {
+            import(/* webpackChunkName: "lightbox" */ '@/Shared/lightbox').then(({default: lightBox}) => {
+                lightBox.normal('.js-lightbox');
+                lightBox.iframe('.js-lightbox-iframe');
+                lightBox.video('.js-lightbox-video');
+            });
+        }
+    },
     // App.common.init runs on all pages
     init() {
-        console.log('jQuery:', $.fn.jquery);
+        console.log('%cINIT', 'padding:0 4px;background:#090;color:#fff', 'jQuery:', $.fn.jquery); // eslint-disable-line no-console
 
-        cookieConsent.init('#cookieContent');
+        cookieConsent.init();
 
-        this.lightbox();
-    },
-    lightbox() {
-        lightbox.normal('.lightbox');
-        lightbox.iframe('.lightbox-iframe');
-        lightbox.video('.lightbox-video');
+        this.dynamicImportModules();
     }
 };
 

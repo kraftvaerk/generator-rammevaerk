5dae1119016a869aadc1dedf1f8390211c330f39
Mithun Dhiman <mi2oon@gmail.com>
2019-01-14 21:21:28 +0100

    Removed plugin-proposal-object-rest-spread (finished proposal)

--- app/templates/_babel.config.js
+++ app/templates/_babel.config.js
@@ -7,7 +7,6 @@ module.exports = {
     ],
     'plugins': [
         '@babel/plugin-transform-modules-commonjs',
-        "@babel/plugin-proposal-object-rest-spread",
         '@babel/plugin-syntax-dynamic-import'
     ]
 };
